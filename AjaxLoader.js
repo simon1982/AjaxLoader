7de41e66e729cf9305670d05a007ea6e06ba02e7
Szymon Domanski <szymon.domanski.2011@gmail.com>
2018-08-05 20:43:35 +0200

    First prototype
    prototype 0.1

---
+++ AjaxLoader.js
@@ -0,0 +1,71 @@
+ function _prototypeGetExternalPageContnetJquery(url,cmd,data == null){
+   console.log('GetExternalPageContnetJquery > url: ' + url);
+   var counter = 10;
+   var step = 0;
+   var result;
+   switch(cmd) {
+      case 'sendPost':
+         $.ajax({
+            url: url,
+            contentType: "text/xml",
+            data : data,
+            dataType: "text",
+            type: 'POST',
+            async: false,
+            error: function() {
+               if(step < 10){
+                  _prototypeGetExternalPageContnetJquery(url,cmd,data);
+                  step++;
+               } else {
+                  alert('system error post data contact admin');
+               }
+            },
+            success: function(data) {
+               console.log('GetExternalPageContnetJquery > url: ')
+               if(!data && step < 10){
+                  _prototypeGetExternalPageContnetJquery(url,cmd,data);
+                  step++;
+                  console.log(step);
+                  console.log(data);
+               } else {
+                  console.log(data);
+                  result = data;
+               }
+            }
+         });
+         return result;
+         break;
+      case 'sendPostSoap':
+        // preper  
+         break;
+      case 'sendGet'
+         $.ajax({
+            url: url,
+            contentType: "text/xml",
+            dataType: "text",
+            type: 'GET',
+            async: false,
+            error: function() {
+               if(step < 10){
+                  GetExternalPageContnetJquery(url);
+                  step++;
+               } else {
+                  alert('system error');
+               }
+            },
+            success: function(data) {
+               console.log('This is success start if')
+               if(!data && step < 10){
+                  GetExternalPageContnetJquery(url);
+                  step++;
+                  console.log(step);
+                  console.log(data);
+               } else {
+                  console.log(data);
+                  result = data;
+               }
+            }
+         });
+         return result;
+         break;
+ }
